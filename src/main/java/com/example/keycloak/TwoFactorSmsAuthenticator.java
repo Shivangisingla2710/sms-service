ce3754dc9d5cc5d56ece89ad634180de7c69af38
Shivangi Singla <shivangisingla2710@gmail.com>
2025-07-29 11:38:59 +0530

    added postgres and time constarint to otp

--- src/main/java/com/example/keycloak/TwoFactorSmsAuthenticator.java
+++ src/main/java/com/example/keycloak/TwoFactorSmsAuthenticator.java
@@ -1,5 +1,7 @@
 package com.example.keycloak;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.models.UserModel;
@@ -7,6 +9,7 @@ import org.keycloak.authentication.AuthenticationFlowError;
 
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
+import jakarta.xml.bind.annotation.XmlValue;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -14,27 +17,28 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 import org.jboss.logging.Logger;
-
 public class TwoFactorSmsAuthenticator implements Authenticator {
 
     private static final Logger logger = Logger.getLogger(TwoFactorSmsAuthenticator.class);
 
-    private static final String API_KEY = "a7091539-50e7-11f0-a562-0200cd936042";
-    private static final String OTP_SESSION_ATTR = "2factor_session";
-    private static final String OTP_VERIFIED_ATTR = "OTP_VERIFIED";
-    private static final String OTP_TIMESTAMP_ATTR = "OTP_TIMESTAMP";
-    // private static final String OTP_RETRIES_ATTR = "OTP_RETRIES";
-    // private static final String OTP_BLOCKED_UNTIL_ATTR = "OTP_BLOCKED_UNTIL";
+    public static final String apiKey = "a7091539-50e7-11f0-a562-0200cd936042";
+    public static final String baseUrl = "https://2factor.in/API/V1/";
+    public static final long otpValidityTime = 1*60*1000;
+    public static final long retryCooldownPeriod = 2*60*1000;
+    public static final int maxRetries = 5;
 
-    // private static final int MAX_RETRIES = 5;
-    private static final long OTP_VALIDITY_MILLIS = 2 * 60 * 1000; // 2 minutes
-    // private static final long RETRY_COOLDOWN_MILLIS = 5 * 60 * 1000; // 5 minutes
+    public static final String OTP_SESSION_ATTR = "2factor_session";
+    public static final String OTP_VERIFIED_ATTR = "OTP_VERIFIED";
+    public static final String OTP_TIMESTAMP_ATTR = "OTP_TIMESTAMP";
 
     @Override
     public void authenticate(AuthenticationFlowContext context) {
         UserModel user = context.getUser();
         String phoneNumber = user.getFirstAttribute("phoneNumber");
         String isAdminCreated = user.getFirstAttribute("isAdminCreated");
+        String blockedUntilStr = user.getFirstAttribute("blockedUntil");
+        String otpRetryCountStr = user.getFirstAttribute("otpRetryCount");
+        String timestampStr = user.getFirstAttribute("lastOtpSentAt");
 
         if (isAdminCreated == null) {
             // Attribute doesn't exist yet, create and set to Yes
@@ -47,24 +51,35 @@ public class TwoFactorSmsAuthenticator implements Authenticator {
 
         Boolean isUpdatePwdRequired = context.getAuthenticationSession().getRequiredActions().contains(UserModel.RequiredAction.UPDATE_PASSWORD.name());
 
-        // String otpVerified = context.getAuthenticationSession().getAuthNote(OTP_VERIFIED_ATTR);
         boolean shouldChallengeOtp = "Yes".equals(isAdminCreated) || isUpdatePwdRequired;
 
         if (shouldChallengeOtp) {
             logger.info("UPDATE PASSWORD REQUIRED IN USER ACTIONS.");
             
-            // long now = System.currentTimeMillis();
-            // String blockedUntilStr = context.getAuthenticationSession().getAuthNote(OTP_BLOCKED_UNTIL_ATTR);
-            // if (blockedUntilStr != null && now < Long.parseLong(blockedUntilStr)) {
-            //     long waitMinutes = (Long.parseLong(blockedUntilStr) - now) / 60000;
-            //     Response challenge = context.form()
-            //         .setError("Too many attempts. Try again in " + waitMinutes + " minutes.")
-            //         .createErrorPage(Response.Status.TOO_MANY_REQUESTS);
-            //     context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR, challenge);
-            //     return;
-            // }
+            long now = System.currentTimeMillis();
+            long blockedUntil = blockedUntilStr != null ? Long.parseLong(blockedUntilStr) : 0;
+            //Block OTP until cooldown period is reached
+            if (blockedUntilStr != null && now < blockedUntil) {
+                long waitMinutes = (blockedUntil - now) / 60000;
+                Response challenge = context.form()
+                    .setError("Too many attempts. Try again in " + waitMinutes + " minutes.")
+                    .createErrorPage(Response.Status.TOO_MANY_REQUESTS);
+                context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR, challenge);
+                return;
+            }
 
+            long otpSentAt = timestampStr != null ? Long.parseLong(timestampStr) : 0;
+            int retries = otpRetryCountStr != null ? Integer.parseInt(otpRetryCountStr) : 0;
+
+            //set retry count and blocked time = 0 after cooldown period is completed
+            if((blockedUntilStr != null && now >= blockedUntil) || ((now - otpSentAt) >= retryCooldownPeriod)){
+                user.setSingleAttribute("blockedUntil", "0");
+                user.setSingleAttribute("otpRetryCount", "0");
+            }
+
+            logger.info("Sending OTP... from authentication method");
             sendOtp(context, phoneNumber);
+
         } else {
             context.success();
         }
@@ -81,14 +96,16 @@ public class TwoFactorSmsAuthenticator implements Authenticator {
 
         String enteredOtp = formData.getFirst("otp");
         String sessionId = context.getAuthenticationSession().getAuthNote(OTP_SESSION_ATTR);
-        String timestampStr = context.getAuthenticationSession().getAuthNote(OTP_TIMESTAMP_ATTR);
-        // String retriesStr = context.getAuthenticationSession().getAuthNote(OTP_RETRIES_ATTR);
+        String timestampStr = context.getUser().getFirstAttribute("lastOtpSentAt");
 
         long now = System.currentTimeMillis();
         long otpSentAt = timestampStr != null ? Long.parseLong(timestampStr) : 0;
-        // int retries = retriesStr != null ? Integer.parseInt(retriesStr) : 0;
+        String otpRetryCountStr = context.getUser().getFirstAttribute("otpRetryCount");
+        int retries = otpRetryCountStr != null ? Integer.parseInt(otpRetryCountStr) : 0;
 
-        if (now - otpSentAt > OTP_VALIDITY_MILLIS) {
+        if ((now - otpSentAt) > otpValidityTime) {
+            retries++;
+            context.getUser().setSingleAttribute("otpRetryCount", String.valueOf(retries));
             Response challenge = context.form()
                 .setError("OTP expired. Please resend OTP.")
                 .createForm("otp.ftl");
@@ -96,19 +113,21 @@ public class TwoFactorSmsAuthenticator implements Authenticator {
             return;
         }
 
-        // if (retries >= MAX_RETRIES) {
-        //     context.getAuthenticationSession().setAuthNote(OTP_BLOCKED_UNTIL_ATTR, String.valueOf(now + RETRY_COOLDOWN_MILLIS));
-        //     Response challenge = context.form()
-        //         .setError("Maximum OTP attempts reached. Please try again after sometime.")
-        //         .createErrorPage(Response.Status.TOO_MANY_REQUESTS);
-        //     context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR, challenge);
-        //     return;
-        // }
+        if (retries >= maxRetries) {
+            context.getUser().setSingleAttribute("blockedUntil", String.valueOf(now + retryCooldownPeriod));
+            Response challenge = context.form()
+                .setError("Maximum OTP attempts reached. Please try again after sometime.")
+                .createErrorPage(Response.Status.TOO_MANY_REQUESTS);
+            context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR, challenge);
+            return;
+        }
 
         try {
             HttpClient client = HttpClient.newHttpClient();
+            String url = baseUrl + apiKey + "/SMS/VERIFY/" + sessionId + "/" + enteredOtp;
+            logger.info("Full URL with OTP is " + url);
             HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://2factor.in/API/V1/" + API_KEY + "/SMS/VERIFY/" + sessionId + "/" + enteredOtp))
+                .uri(URI.create(url))
                 .GET()
                 .build();
 
@@ -119,15 +138,16 @@ public class TwoFactorSmsAuthenticator implements Authenticator {
                 context.getAuthenticationSession().setAuthNote(OTP_VERIFIED_ATTR, "true");
                 context.success();
                 context.getUser().setSingleAttribute("isAdminCreated", "No");
+                //set retry count and blocked time = 0 in case of successful otp validation
+                context.getUser().setSingleAttribute("otpRetryCount", "0");
+                context.getUser().setSingleAttribute("blockedUntil", "0");
                 logger.info("isAdminCreated set to No");
             } else {
-                // retries++;
-                // context.getAuthenticationSession().setAuthNote(OTP_RETRIES_ATTR, String.valueOf(retries));
-                context.getAuthenticationSession().setAuthNote("OTP_LAST_ATTEMPT_TIME", String.valueOf(System.currentTimeMillis()));
+                retries++;
+                context.getUser().setSingleAttribute("otpRetryCount", String.valueOf(retries));
 
                 Response challenge = context.form()
-                    // .setError("Invalid OTP. Attempt " + retries + " of " + MAX_RETRIES)
-                    .setError("Invalid OTP. Please Try again")
+                    .setError("Invalid OTP. Attempt " + retries + " of " + maxRetries)
                     .createForm("otp.ftl");
                 context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challenge);
             }
@@ -141,25 +161,6 @@ public class TwoFactorSmsAuthenticator implements Authenticator {
 
     private void sendOtp(AuthenticationFlowContext context, String phoneNumber) {
 
-        // int attempts = Integer.parseInt(context.getAuthenticationSession().getAuthNote("OTP_ATTEMPTS") != null ? context.getAuthenticationSession().getAuthNote("OTP_ATTEMPTS") : "0");
-        // String lastAttemptTimeStr = context.getAuthenticationSession().getAuthNote("OTP_LAST_ATTEMPT_TIME");
-        // long now_1 = System.currentTimeMillis();
-
-        // if (attempts >= MAX_RETRIES && lastAttemptTimeStr != null) {
-        //     long lastAttemptTime = Long.parseLong(lastAttemptTimeStr);
-        //     if ((now_1 - lastAttemptTime) < RETRY_COOLDOWN_MILLIS) {
-        //         Response challenge = context.form()
-        //             .setError("Maximum attempts reached. Please try again after some time.")
-        //             .createErrorPage(Response.Status.TOO_MANY_REQUESTS);
-        //         context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challenge);
-        //         return;
-        //     } else {
-        //         // Reset attempts after cooldown
-        //         context.getAuthenticationSession().setAuthNote("OTP_ATTEMPTS", "0");
-        //         context.getAuthenticationSession().removeAuthNote("OTP_LAST_ATTEMPT_TIME");
-        //     }
-        // }
-
         if (phoneNumber == null || phoneNumber.isEmpty()) {
             Response challenge = context.form()
                 .setError("Missing phone number.")
@@ -170,29 +171,29 @@ public class TwoFactorSmsAuthenticator implements Authenticator {
 
         try {
             HttpClient client = HttpClient.newHttpClient();
+            String url = baseUrl + apiKey + "/SMS/" + phoneNumber + "/AUTOGEN";
+            logger.info("Sending OTP via URL: " + url);  // ADD THIS LINE
             HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://2factor.in/API/V1/" + API_KEY + "/SMS/" + phoneNumber + "/AUTOGEN"))
+                .uri(URI.create(url))
                 .GET()
                 .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             String body = response.body();
 
+            logger.info("Response Body is: " + body);
+
             String sessionId = body.split("Details\":\"")[1].split("\"")[0];
-            long now = System.currentTimeMillis();
 
             context.getAuthenticationSession().setAuthNote(OTP_SESSION_ATTR, sessionId);
-            context.getAuthenticationSession().setAuthNote(OTP_TIMESTAMP_ATTR, String.valueOf(now));
-            // 👇 Do not reset retries here!
-            // if (context.getAuthenticationSession().getAuthNote(OTP_RETRIES_ATTR) == null) {
-            //     context.getAuthenticationSession().setAuthNote(OTP_RETRIES_ATTR, "0");
-            // }
             context.getAuthenticationSession().setAuthNote(OTP_VERIFIED_ATTR, "false");
+            context.getUser().setSingleAttribute("lastOtpSentAt", String.valueOf(System.currentTimeMillis()));
 
             Response challenge = context.form()
                 .createForm("otp.ftl");
             context.challenge(challenge);
         } catch (Exception e) {
+            logger.error("Failed to send OTP: " + e.getMessage(), e);
             Response challenge = context.form()
                 .setError("Failed to send OTP.")
                 .createErrorPage(Response.Status.INTERNAL_SERVER_ERROR);
@@ -203,6 +204,14 @@ public class TwoFactorSmsAuthenticator implements Authenticator {
     private void resendOtp(AuthenticationFlowContext context) {
         UserModel user = context.getUser();
         String phoneNumber = user.getFirstAttribute("phoneNumber");
+        int retries = context.getUser().getFirstAttribute("otpRetryCount") != null ? Integer.parseInt(context.getUser().getFirstAttribute("otpRetryCount")) : 0;
+        if(retries > maxRetries){
+            Response challenge = context.form()
+            .setError("Maximum OTP attempts reached. Please try again after sometime.")
+            .createErrorPage(Response.Status.TOO_MANY_REQUESTS);
+            context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR, challenge);
+            return;
+        }
         logger.info("Resending OTP...");
         sendOtp(context, phoneNumber);
     }
