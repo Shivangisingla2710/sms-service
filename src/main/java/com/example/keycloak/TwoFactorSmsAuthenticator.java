940fbc9331e30ba4cd12682aa7bf05fd819397c3
Shivangi Singla <shivangisingla2710@gmail.com>
2025-07-30 17:54:50 +0530

    configured ngrok for remote use

--- src/main/java/com/example/keycloak/TwoFactorSmsAuthenticator.java
+++ src/main/java/com/example/keycloak/TwoFactorSmsAuthenticator.java
@@ -19,7 +19,7 @@ public class TwoFactorSmsAuthenticator implements Authenticator {
 
     private static final Logger logger = Logger.getLogger(TwoFactorSmsAuthenticator.class);
 
-    public static final String apiKey = "a7091539-50e7-11f0-a562-0200cd936042";
+    public static final String apiKey = "<YOUR-API-KEY>";
     public static final String baseUrl = "https://2factor.in/API/V1/";
     public static final long otpValidityTime = 10*60*1000; // 10 minutes
     public static final long retryCooldownPeriod = 60*60*1000; // 1 hour
